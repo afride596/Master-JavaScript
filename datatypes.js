768e2b33de34570642876fc0ad841b2f76f08379
afide596 <afride596@gmail.com>
2023-08-22 22:56:33 +0530

    datatype

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as  newer version
+
+alert("hello") // wee are using nodejs ,not browers
+
+//  code readability should be High/
+
+let name='afridi'
+let age=18
+let loggedin=false
+
+
+// premitive datatype
+Number
+BigInt
+String
+Boolean
+null
+undefined
+Symbol (ES6)
+
+// object data type
+Object
+ 
