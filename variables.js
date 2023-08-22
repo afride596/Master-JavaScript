5f14ca9018ad17cd90947e4c4c78e4d69797c6c5
afide596 <afride596@gmail.com>
2023-08-22 22:34:51 +0530

    completed variables

---
+++ variables.js
@@ -0,0 +1,14 @@
+const accountId=145536
+let accountEmail="afride@gmail.com"
+var accountPassword="1234"
+accountCity="rajasthan"
+
+// accountId=56455// not allowed to change the id of an existing user
+
+// console.log(accountId);
+
+accountEmail="hitesh@hitesh.com"
+accountPassword="56466"
+accountCity="hyderabad"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
