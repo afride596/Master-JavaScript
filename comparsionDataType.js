c6aa13a10860ff0c56a43d81e883e42638aeb5db
afide596 <afride596@gmail.com>
2023-08-23 23:17:22 +0530

    stack and heap

--- comparsionDataType.js
+++ comparsionDataType.js
@@ -20,4 +20,27 @@
 // nonPremtive dataType 
 // Array object,Function 
 
+// stack(premitive),
 
+// let youtubeName='afrideMohammad'
+//  let anotherName=youtubeName;
+//  console.log(anotherName)
+//  anotherName="mohammadafride"
+//  console.log(anotherName);
+
+//  Heap(non-primitive)
+
+let UserAccount1={
+    email:"afride@google.com",
+    upi:"ause@icicibank"
+
+
+}
+let UserAccount2=UserAccount1;
+
+ UserAccount2={
+    email:'afride@hdfc.com',
+    upi:'ididi@idfcbank'
+}
+UserAccount1.email="afride@hdfcbank.com"
+console.log(UserAccount1.upi)
