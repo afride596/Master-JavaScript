839209be0e426115cdf4a42deba86643084ad0c4
afide596 <afride596@gmail.com>
2023-08-23 15:37:53 +0530

    converison of datatype

---
+++ conversionOperations.js
@@ -0,0 +1,29 @@
+let score ="33fs"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber=Number(score)
+console.log( typeof valueInNumber)
+console.log( valueInNumber)
+
+// "33" =>33
+// "33abc" =>NaN
+// true =>1; False =>0 
+
+if(isloggedin=""){
+    // let isloggedin=1
+ let isbolleanLogin=Boolean(isloggedin)
+ console.log(isbolleanLogin);
+
+}
+else{
+    // let isloggedinn=0
+    let isbolleanLogin=Boolean(isloggedin)
+    console.log(isbolleanLogin);
+}
+// 1=>true in Boolean
+// 0=>false in boolean
+
+// 33 =>String(33)="33"
