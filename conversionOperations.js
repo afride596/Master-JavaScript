f0724cf7424ce41f3e7fab9ba64b38e1bf67d963
afide596 <afride596@gmail.com>
2023-08-23 16:05:00 +0530

    operatrations Done

--- conversionOperations.js
+++ conversionOperations.js
@@ -27,3 +27,25 @@ else{
 // 0=>false in boolean
 
 // 33 =>String(33)="33"
+
+// ***************************operation***********************
+
+let value=3
+let negValue= -value
+
+console.log(negValue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 ="hello\n"
+let str2 ="world"
+
+let str3=str1 + str2
+console.log(str3);
+
+console.log(+false)
