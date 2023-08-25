5c181b11b4f4e5ed9ed45e8c05523de8a3f9c465
afide596 <afride596@gmail.com>
2023-08-25 22:31:55 +0530

    math And number

---
+++ NumberMath.js
@@ -0,0 +1,30 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>NUMBER>>>>>>>>>>>>>>>>>>>>>>
+const Score =400
+
+const balance= new Number(2354)//==>> //number constructor
+console.log(`Balance: ${balance}`)
+console.log(balance.toFixed(1))
+console.log(balance.toString().length)
+console.log(balance.toString().split(4))
+
+const newBalance=499.5640
+
+console.log(newBalance.toPrecision(3))
+
+const updatedBalance=1000000
+console.log(updatedBalance.toLocaleString('en-IN'));
+console.log(updatedBalance.valueOf());
+console.log(updatedBalance.toString().length);
+console.log(updatedBalance)
+
+// ++++++++++++++++++++++++++++Maths++++++++++++++++++
+console.log(Math.PI);
+console.log(Math.sqrt(625));
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.9));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,3));
+console.log(Math.max(4,9));
+console.log(Math.random());
+
