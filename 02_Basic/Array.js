860ca5ddb8b7553dc3685bc66b65d2b2ffdc9ca1
afide596 <afride596@gmail.com>
2023-08-28 21:09:36 +0530

    Array part1

--- 02_Basic/Array.js
+++ 02_Basic/Array.js
@@ -1,4 +1,43 @@
 // ARRAY 
 
 
-const myarr=[0,1,2,3,4,5]  
+const myarr=[1,2,3,4,5]  
+const hero=["hero","nakizantajar"]
+
+const array2=new Array(1,2,3,4)
+// console.log()
+
+console.log(myarr[3])
+
+const method1=array2.length
+console.log(method1);
+
+const method2=array2.push(6,7)
+console.log(method2);
+
+const method3=array2.pop()
+console.log(method3);
+
+const method4=array2.unshift(9)
+console.log(method4);
+
+const method5=array2.shift()
+console.log(method5)
+
+const method6=array2.includes(6)
+console.log(method6);
+
+const method7=array2.indexOf(2)
+console.log(method7);
+
+const method8 =array2.join() //==>it will bind and convert into string
+console.log(method8);
+console.log(typeof method8);
+
+const method9 =array2.slice(0,4)
+console.log("sliced",method9 );
+
+const method10 =array2.splice(0,3)
+console.log(method10);
+
+console.log(array2)
