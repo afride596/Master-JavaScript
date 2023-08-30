4f16cafbd1b9e9d5840d9d316b674dcabe6b0424
afide596 <afride596@gmail.com>
2023-08-30 12:24:08 +0530

    objects

--- 02_Basic/objects.js
+++ 02_Basic/objects.js
@@ -1,1 +1,34 @@
-// si
+// singleton
+
+// object literals 
+const mysym = Symbol("key1")
+
+const jsuser={
+    'name':"afride",
+    age:24,
+    [mysym]:"key22",
+    location:"vijayawada",
+    isloggedin:false,
+    "email":"afride@google.com",
+    lastLoginday:["monday","friday"]
+
+
+}
+// console.log(jsuser.email);
+console.log(jsuser["email"])
+console.log(jsuser[mysym])
+
+// Object.freeze(jsuser)
+jsuser.email="ad"
+console.log(jsuser);
+jsuser.greeting=function(){
+    console.log("hello Js user");
+
+}
+jsuser.greetingTwo=function(){
+    console.log(`hello js user,${this.name}`);
+
+
+} 
+console.log(jsuser.greeting());
+console.log(jsuser.greetingTwo());
