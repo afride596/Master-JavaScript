2b1e1b5e6ed24b98a33365b296a50dadacae2254
afide596 <afride596@gmail.com>
2023-11-04 00:28:39 +0530

    completed forloop iteration

---
+++ 04_control Flow/one.js
@@ -0,0 +1,68 @@
+let a=5
+let b=8
+if(a>b){
+    // console.log(`printing the numnber that want to showcase the hole life ${a}`);
+}else{
+    // console.log(`this is not working then print ${b}` );
+}
+
+// const balance=1000
+// if(balance<500){
+//     console.log("less than 500");
+// }else if(balance<750){
+//     console.log("between 500 and 749");
+// }else if(balance<900){
+//     console.log("between 750 and 899");
+// }else if(balance===1000){
+//     console.log("between 900 and 999");
+// }
+
+// const userLoggedIn=true
+const debitCard=true
+const loggedInFromGoogle=false
+const loggedInFromGmail=true
+
+// if(userLoggedIn&&debitCard){
+//     console.log("Allow to shopping")
+// }
+
+// if(loggedInFromGmail||loggedInFromGoogle){
+//     console.log("You are logged in from GMAIL or GOOGLE")
+// }
+
+// // switch 
+//  const month =2
+// switch(month){
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("Febuary")
+//     case 3:
+//         console.log("march")
+//         break
+//     default:
+//         console.log("End of switch statement")
+// }
+const userLoggedIn=[]
+// const userLoggedIn
+if(userLoggedIn){
+    console.log("User Logged In")
+}else{
+    console.log("Not logged in")
+}
+
+// falsy values 
+// false,0,-0,BigInt 0n,"",underfined,NaN)
+
+// true values 
+// "0",'false'," ",[],{},function(){}
+
+// if(userLoggedIn.length===0){
+//     console.log("array is empty")
+// }
+
+let emptyObject={}
+if(Object.keys(emptyObject).length==0){
+    console.log("object is empty")
+}
---
+++ 05_iteration/one.js
@@ -0,0 +1,40 @@
+//For loop
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+// const array=["spirt","hallow","weean"]
+// for (let i = 0; i < array.length; i++) {
+//     // const element = array[i];
+//     // console.log(element);
+//     console.log(`${array[i]}is weaking ${array[i+1]}`);
+    
+// }
+
+// let array=[10]
+// let j=10;
+
+// Tables 
+
+for (let index = 1; index <=10; index++) {
+    console.log(`Now the table number is ${index}`)
+    for (let j = 1; j <=10; j++) {
+        console.log(`${index} X ${j}=${index*j}`)
+
+        
+    }
+}
+
+for(let i=0;i<=10;i++){
+    const element=i;
+    if(element==5){
+        console.log(`please give free to ${element}`);
+    }
+    console.log(element);
+    
+}
+
+
+
+
