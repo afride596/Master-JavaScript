91575d2fb0e2a13a472522eb3c673a350d6ee09b
afride596 <afride596@gmail.com>
2024-08-29 17:21:11 +0530

    functions

--- 03_Basic/function.js
+++ 03_Basic/function.js
@@ -5,41 +5,34 @@
 // console.log("D");
 // console.log("E\n");
 
-function sayMyName(){
-    console.log("A");
-console.log("F");
-console.log("R");
-console.log("I");
-console.log("D");
-console.log("E\n");
+function sayMyName() {
+  console.log("A");
+  console.log("F");
+  console.log("R");
+  console.log("I");
+  console.log("D");
+  console.log("E\n");
 }
 // sayMyName();
 // sayMyName();
 // sayMyName();
 // sayMyName();
 
-function addTwoNumber(num1,num2){
-    let res=num1+num2
-    console.log(res);
-    // return num1+num2
-    // return res
+function addTwoNumber(num1, num2) {
+  let res = num1 + num2;
+  console.log(res);
+  // return num1+num2
+  // return res
 }
 // let res =addTwoNumber(2,5)
 // addTwoNumber(2,5)
 // console.log(res)
 
-function loginUserMessage(username="afridde"){
-    if (!username){
-        return "Welcome Admin"
-        }else{
-            return `welcome ${username}`
-            }
-              
-              please(){
-                welcome to your pc 
-              }
-              console.log(Hello my name is afride);
+function loginUserMessage(username = "afridde") {
+  if (!username) {
+    return "Welcome Admin";
+  } else {
+    return `welcome ${username}`;
+  }
 }
 console.log(loginUserMessage());
-
-
